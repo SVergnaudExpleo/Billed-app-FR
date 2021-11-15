8e7f2b82cefb87978b6f1e2f720fa36a15747ebe
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-11-15 10:25:26 +0100

    (test) complete container/Bills test coverage to 62%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,14 +1,27 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import Bills from "../containers/Bills"
+import firebase from "../__mocks__/firebase"
 
 describe("Given I am connected as an employee", () => {
+  // connection comme employée //
+  beforeEach(()=>{Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({
+      type: 'Employee'
+    }))}
+  );
   describe("When I am on Bills Page", () => {
+
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
+      const html = BillsUI({ data: bills})
       document.body.innerHTML = html
-      //to-do write expect expression
-    })
+      const icone = screen.getByTestId("icon-window")
+      expect(icone).toBeTruthy()
+      expect(icone.className).toBe("active-icon")
+    });
+
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
@@ -16,6 +29,44 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
-    })
-  })
-})
+    });
+  
+    test("Then user click on new bill button, buttton is called", ()=>{
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = BillsUI({ data: bills })
+      }
+      const firestore = null
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+
+      const billContainer = new Bills({document,onNavigate,firestore,LocalStorage: window.localStorage})
+      expect(billContainer).toBeTruthy
+      const buttonNewbill = screen.getByTestId("btn-new-bill")
+      expect (buttonNewbill).toBeTruthy
+      buttonNewbill.click()
+      expect (buttonNewbill).toBeCalled
+    });
+
+    test("Then user click on eyes button, button is called",()=>{
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = BillsUI({ data: bills })
+      }
+      const firestore = null
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      const billContainer = new Bills({document,onNavigate,firestore,LocalStorage: window.localStorage})
+      expect(billContainer).toBeTruthy
+      const buttonEye = screen.getAllByTestId("icon-eye")
+      expect (buttonEye).toBeTruthy
+      buttonEye[0].click()
+      expect(buttonEye).toBeCalled
+    });
+  });
+
+  describe("When I navigate to bils", () => {
+    test("fetches bills from mock API GET", async () => {
+       const getSpy = jest.spyOn(firebase, "get")
+       const bills = await firebase.get()
+       expect(getSpy).toHaveBeenCalledTimes(1)
+       expect(bills.data.length).toBe(4)
+    });
+  });
+});
