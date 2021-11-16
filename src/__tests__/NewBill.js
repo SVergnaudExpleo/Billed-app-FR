d3895adee2a80bdec405ac767d5bc426b85058ce
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-11-16 14:33:36 +0100

    (test) newbill container test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,99 @@
-import { screen } from "@testing-library/dom"
+import { screen, fireEvent } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { ROUTES } from "../constants/routes"
+import firebasePost from "../__mocks__/firebasePost"
 
 describe("Given I am connected as an employee", () => {
+
+  // connection comme employée //
+  beforeEach(()=>{Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({
+      type: 'Employee'
+    }))}
+  );
+
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then user set wrong file format, alert must be display", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const onNavigate = (pathname) => {
+        null
+      };
+      const uiOk = screen.getByTestId("form-new-bill");
+      expect(uiOk).toBeTruthy;
+      const firestore = null;
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+      window.alert = jest.fn()
+
+      const newBillContainer = new NewBill({document,onNavigate,firestore,LocalStorage: null});
+      expect(newBillContainer).toBeTruthy;
+      const file = screen.getByTestId("file");
+      expect(file).toBeTruthy;
+      fireEvent.change(file, {
+        target: {
+          files: [new File(['file content'],'mauvais-type.txt',{type: 'text/plain', name: 'mauvais-type.txt'})],
+        }
+      })
+      expect(window.alert).toBeCalled()
+    });
+
+    test("Then user set the good file format, no alert must be display", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      };
+      Object.defineProperty(window, 'firebase', { value: localStorageMock })
+      window.firebase.storage('storage', JSON.stringify({
+        value: 'toto',
+      }));
+
+      class Firestore {
+        constructor() {
+          this.store = window.firebase ? window.firebase.firestore() : () => null
+          this.storage = window.firebase ? window.firebase.storage() : () => null
+        }
+      };
+      const uiOk = screen.getByTestId("form-new-bill");
+      expect(uiOk).toBeTruthy;
+      window.alert = jest.fn();
+
+      const newBillContainer = new NewBill({document,onNavigate,firestore: Firestore,LocalStorage: window.localStorage});
+      expect(newBillContainer).toBeTruthy;
+      const file = screen.getByTestId("file");
+      expect(file).toBeTruthy;
+      fireEvent.change(file, {
+        target: {
+          files: [new File(['file content'],'bon-type.png',{type: 'image/png', name:'bon-type.png'})],
+        }
+      })
+      expect(window.alert).not.toBeCalled()
+    });
+
+    test("Then on submit, the form must be submit", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      };
+      const newBillContainer = new NewBill({document,onNavigate,firestore: null,LocalStorage: window.localStorage});
+      const form = screen.getByTestId("form-new-bill");
+      expect(form).toBeTruthy;
+      fireEvent.submit(form)
+      expect(form.onsubmit).toBeTruthy;
+    });
+  });
+});
+
+describe("Given I am a user connected as Employee", () => {
+  describe("When I navigate to NewBills", () => {
+    test("fetches bills from mock API POST", async () => {
+        const getSpy = jest.spyOn(firebasePost, "post")
+        const bills = await firebasePost.post()
+        expect(getSpy).toHaveBeenCalledTimes(1)
+        expect(bills.data.length).toBe(4)
     })
   })
 })
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -18,8 +18,23 @@ export default class NewBill {
   handleChangeFile = e => {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    this.firestore
+    //const fileName = filePath[filePath.length-1]
+    
+    const fileName = file.name
+    const tableAutorizedFile = [".jpeg", ".jpg", ".png"]
+    var compteur = 0
+    tableAutorizedFile.forEach(function (fileType) {
+      var fileTypeRegex = new RegExp(fileType, 'i')
+      if (fileName.search(fileTypeRegex) <= -1) {
+        compteur++
+      }
+    });
+    if (compteur == tableAutorizedFile.length) {
+      var fileArea = document.querySelector(`input[data-testid="file"]`)
+      fileArea.value = ""
+      alert("le type de fichier doit être ,.jpeg, .jpg, .png.\r\ Vous avez utilisé le fichier ci-dessous \r\ \r\ " + fileName)
+    } else {
+      this.firestore
       .storage
       .ref(`justificatifs/${fileName}`)
       .put(file)
@@ -28,7 +43,9 @@ export default class NewBill {
         this.fileUrl = url
         this.fileName = fileName
       })
+    }
   }
+  
   handleSubmit = e => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
